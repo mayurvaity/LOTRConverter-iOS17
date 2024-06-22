b29268de5d6c3b2175597f623f5846d97b969471
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-06-22 14:59:58 +0530

    Initial Commit

---
+++ LOTRConverter-iOS17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRConverter-iOS17
+//
+//  Created by Mayur Vaity on 22/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter-iOS17/LOTRConverter_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter_iOS17App.swift
+//  LOTRConverter-iOS17
+//
+//  Created by Mayur Vaity on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
